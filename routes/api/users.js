f9d34b9ca06c32261527ea538d3db4e8e843e128
Chloe <1641302270@qq.com>
2022-01-20 16:20:58 +0800

    接口设计

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,18 +3,24 @@ const express = require("express")
 const router = express.Router()
 // 密码加密
 const bcrypt = require("bcrypt")
+// 获取token
+const jwt = require('jsonwebtoken');
+const keys = require("../../config/keys")
+// 验证token
+const passport = require("passport")
 
 // 引入User.js
-const User = require("../../models/User")
+const User = require("../../models/User");
 
-// $route   GET /api/users/test
+// $route   GET /users/test
 // @desc    返回请求的json数据 用于测试
 // @access  public
 router.get("/test", (req, res) => {
     res.json({ msg: "login works" })
 })
 
-// $route   POST /api/users/register
+
+// $route   POST /users/register
 // @desc    返回用户注册的json数据
 // @access  public
 router.post("/register", (req, res) => {
@@ -33,7 +39,7 @@ router.post("/register", (req, res) => {
 
                 // 密码加密
                 bcrypt.genSalt(10, function (err, salt) {
-                    bcrypt.hash(newUser.password, salt, function (err, hash) {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if (err) throw err
                         // 将hash值赋给密码进行加密
                         newUser.password = hash
@@ -47,6 +53,47 @@ router.post("/register", (req, res) => {
         })
 })
 
+// $route   POST /users/login
+// @desc    返回用户登录的token jwt passport
+// @access  public
+router.post("/login", (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    User.findOne({ username })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ msg: "用户不存在" })
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const rule = { id: user.id, username: user.username }
+                        jwt.sign(rule, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                            if (err) throw err
+                            res.json({
+                                success: true,
+                                token: "Bearer " + token
+                            })
+                        })
+                        // res.json({ msg: "success" })
+                    } else {
+                        return res.status(400).json({ msg: "密码错误" })
+                    }
+                })
+        })
+})
+
+// $route   GET /users/current
+// @desc    返回 current user
+// @access  private
+router.get("/current", passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        username: req.user.username
+    })
+})
+
 module.exports = router
 
 // 1. 引入router
@@ -57,3 +104,8 @@ module.exports = router
 
 // post 需要插件body-parser 在server.js中引入 使用body-parser中间件
 // 密码加密 需要插件bcrypt
+
+// 获取token 需要jsonwebtoken插件 引入后使用jwt.sign("规则","加密名字","到期时间","箭头函数")
+// 加密名字在 /config/keys.js 中写
+
+// 验证token 需要passport和passport-jwt插件 在server.js中引入 并初始化 在passport.js中配置
--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// 引入验证token所需的passport 在连接数据库后初始化并配置
+const passport = require("passport")
+
 // 引入users的api
 const users = require("./routes/api/users")
+// 引入products的api
+const products = require("./routes/api/products")
+// 引入ads的api
+const ads = require("./routes/api/ads")
 
 // 连接数据库: 从./config/keys.js中获取URI
 const db = require("./config/keys").mongoURI
@@ -22,13 +29,22 @@ mongoose.connect(db)
     })
     .catch(err => { console.log(err); })
 
-//设置路由, 使得在浏览器中可以访问
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+// 初始化passport 并传入到passport.js中 实现代码分离
+app.use(passport.initialize());
+require("./config/passport")(passport)
 
-// 使用routes: 当浏览器访问 /api/users的时候会使用上面引入的users的api
-app.use("/api/users", users)
+// //设置路由, 使得在浏览器中可以访问
+// app.get("/", (req, res) => {
+//     res.send("Hello World!")
+// })
+
+// 使用routes: 当浏览器访问 /users的时候会使用上面引入的users的api
+// url自动拼接为 http://localhost:5000/users/
+app.use("/users", users)
+// url自动拼接为 http://localhost:5000/products/
+app.use("/products", products)
+// url自动拼接为 http://localhost:5000/ads/
+app.use("/ads", ads)
 
 // 监听端口
 const port = process.env.PORT || 5000;
